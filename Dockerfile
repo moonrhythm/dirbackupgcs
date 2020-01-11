1424bc6e13cf1bbcb8c82dff6228e77385802b2f
Thanatat Tamtan <acoshift@gmail.com>
2020-01-11 16:40:43 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM alpine
+
+RUN apk add --no-cache curl bash python && \
+	curl https://sdk.cloud.google.com | bash && \
+	rm -rf /root/.config
+
+ADD backup.sh /
+RUN chmod +x /backup.sh
+
+ENTRYPOINT ["/backup.sh"]
---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export PATH=$PATH:/root/google-cloud-sdk/bin
+
+if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" ]]; then
+  echo $GOOGLE_APPLICATION_CREDENTIALS_JSON > /root/google-credential.json
+  gcloud auth activate-service-account --key-file /root/google-credential.json
+fi
+
+tar -zcvf - /data | gsutil cp - gs://$BUCKET/$PREFIX$(date +"%Y%m%d%H%M%S")$SUFFIX.tar.gz
